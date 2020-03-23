e3e1b4c1a1eb051a02550229226b14bd0324d929
theob0t <theo06bot75@gmail.com>
2020-03-23 06:36:02 -0700

    Snake_Game

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from cx_Freeze import setup, Executable
 
+build_exe_options = {"excludes": ["tkinter", "PyQt4.QtSql", "sqlite3", 
+                                  "scipy.lib.lapack.flapack",
+                                  "PyQt4.QtNetwork",
+                                  "PyQt4.QtScript",
+                                  "numpy.core._dotblas", 
+                                  "PyQt5"],
+                     "optimize": 2}
+
 setup(name = "Snake_Game" ,
       version = "0.1" ,
       description = "" ,
+      options = {"build_exe": build_exe_options},
+      data_files=[('data', ['./data/score.txt','./data/logo.jpg'])],
       executables = [Executable("Snake_Game.py", base = "Win32GUI")])
